933d75a7ec93c41115c550e55ccbf3c296b607a7
sajan <sajanbaisil12@gmail.com>
2022-08-08 16:58:31 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      title: 'D A S H B O A R D',
         debugShowCheckedModeBanner: false,
         home: ResponsiveScreen(
             Desktopscreen: DesktopScreen(),
